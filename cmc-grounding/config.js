d5b38a00297ca10435da2aa5d4183321a526110e
Katie Smith <k3smith@odu.edu>
2026-07-25 17:59:31 -0400

    set google sheet

--- cmc-grounding/config.js
+++ cmc-grounding/config.js
@@ -3,7 +3,7 @@ window.GROUNDING_CONFIG = {
   roundId: "cmc_grounding_pilot",
   roundTitle: "CMC grounding pilot",
   atomsUrl: "atoms/cmc_grounding_pilot.json",
-  sheetWebAppUrl: "",
+  sheetWebAppUrl: "https://script.google.com/macros/s/AKfycbzkd1_Ts5zcIJ1TQvqiMj1ub1m5lwV0hl1SkMACeADjA4SHd6EI34DnJ8Jzot77vGxQGA/exec",
   /** Atom stops being offered once this many distinct raters finished it. */
   targetRatings: 1,
   /** Frameworks that must have a match before Save. */
